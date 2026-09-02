e7d678e9150a19ab1222df08407db960acd283f1
Muhammad Ahmed Mohsin <muhammadahmedmohsin123@gmail.com>
2026-09-02 14:54:05 +0500

    code changes according to vercel

--- Frontend/src/services/api.jsx
+++ Frontend/src/services/api.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://muhammadahmed144-stripe-payment-int.vercel.app/",
 });
 
