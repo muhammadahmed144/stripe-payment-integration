c81e217461baab994410cd239a9c6441d4265140
Muhammad Ahmed Mohsin <muhammadahmedmohsin123@gmail.com>
2026-09-02 17:28:22 +0500

    code changes according to vercel errors

--- Frontend/src/services/api.jsx
+++ Frontend/src/services/api.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://stripe-payment-integration-gamma.vercel.app",
+  baseURL: "https://stripe-payment-integration-gamma.vercel.app/api",
 });
 
