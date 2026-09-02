4501615be3b7703163b46b7f90dd9c388e8d69df
Muhammad Ahmed Mohsin <muhammadahmedmohsin123@gmail.com>
2026-09-02 21:45:23 +0500

    code changes according to secure baseURL

--- Frontend/src/services/api.jsx
+++ Frontend/src/services/api.jsx
@@ -1,6 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://stripe-payment-integration-gamma.vercel.app/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
 });
-
